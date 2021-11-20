7375b8279e0722e38e67105bc8ee6ad551a797e9
Sebastian Garcia <eldraco@gmail.com>
2021-11-20 19:37:06 +0100

    more

--- camera-robot.py
+++ camera-robot.py
@@ -1,33 +1,69 @@
 #!/usr/bin/env python3
+# Pi-camera-robot
+# Author: eldraco@gmail.com
+
 import RPi.GPIO as GPIO
 from time import sleep
+import argparse
 
-# this sets the names to board mode, which just names the pins according to the numbers in the middle of the diagram above.
-GPIO.setmode(GPIO.BOARD)
+SERVO_HORIZ_IO = 3
+SERVO2_VERT_IO = 5
 
+def init_rpi():
+    """
+    Initialize the rpi io
+    """
+    # this sets the names to board mode, which just names the pins according to the numbers in the middle of the diagram above.
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(3, GPIO.OUT)
+    # Now setup PWM on pin #3 at 50Hz
+    pwm = GPIO.PWM(3, 50)
+    #Then start it with 0 duty cycle so it doesn't set any angles on startup
+    pwm.start(0)
 
-GPIO.setup(3, GPIO.OUT)
+def stop_rpi():
+    """
+    Stop the rpi
+    """
+    pwm.stop()
+    GPIO.cleanup()
 
-# Now setup PWM on pin #3 at 50Hz
-pwm = GPIO.PWM(3, 50)
-
-#Then start it with 0 duty cycle so it doesn't set any angles on startup
-pwm.start(0)
-
-# to set the angle of the servo, we need to send a specific signal to it. This can differ from servo to servo, as normally it's from 2.5-12.5%, and on the ones I'm using it's 2-12%. Regardless, it will be a 10% window, so to calculate the duty cycle for your desired angle, divide by 18, then add the lowest available value, in this case 2
-
-
-def SetAngle(angle):
+def set_angle(angle, direction='base'):
+    """
+    Set the angle on servo X
+    direction can be 'horizontal' or 'vertical'
+    """
     duty = angle / 18 + 2
-    GPIO.output(3, True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(3, False)
-    pwm.ChangeDutyCycle(0)
+    if direction == 'horizontal':
+        # Move servo horizontal
+        GPIO.output(SERVO_HORIZ_IO, True)
+        pwm.ChangeDutyCycle(duty)
+        sleep(1)
+        GPIO.output(SERVO_HORIZ_IO, False)
+        pwm.ChangeDutyCycle(0)
+    elif direction == 'vertical':
+        GPIO.output(SERVO2_VERT_IO, True)
+        pwm.ChangeDutyCycle(duty)
+        sleep(1)
+        GPIO.output(SERVO2_VERT_IO, False)
+        pwm.ChangeDutyCycle(0)
 
 
-SetAngle(90) 
+####################
+# Main
+####################
+if __name__ == '__main__':
+    print('Pi-camera-robot.')
 
-pwm.stop()
-GPIO.cleanup()
+    # Parse the parameters
+    parser.add_argument('-v', '--verbose',metavar='<verbositylevel>',action='store', required=False, type=int, help='amount of verbosity. This shows more info about the results.')
 
+    args = parser.parse_args()
+
+    init_rpi()
+
+    # Move servo 1
+    for angle in range(0,100,10):
+        set_angle('horizontal', angle) 
+
+    stop_rpi()
